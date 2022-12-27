474fc182484f5c88248581e434edb50276fc3ca5
Suraj12398 <surajdeosarkar99@gmail.com>
2022-12-27 14:18:44 +0530

    print_report_card

---
+++ assignment4.js
@@ -0,0 +1,49 @@
+
+let head1="SUBJECT       "
+let head2="TOTAL MARKS   "
+let totalp=" (100) "
+let totalm="(100)"
+let totalc=" (100)   "
+let head3="OBTAINED MARKS"
+let marksphysics="   (97)"
+let marksmaths=" (99)"
+let markschemistry="   (98)  "
+let hmarksphysics="PHYSICS"
+let hmarksmaths="MATHS"
+let hmarkschemistry="CHEMISTRY"
+
+let name="Suraj Subhashrao Deosarkar"
+let school="           Sᴄʜᴏᴏʟ Oғ Sᴄʜᴏʟᴀʀs"
+let grade=" Grade :- (A+)"
+let section="  Section:- A   "
+let rollno="Roll No :- 172   "
+let sign="            Signature Of Authority:-"
+let actsign="𝓀𝒶𝓅ℴℴ𝓇"
+let space=" "
+let lowline="______________________________________________"
+let line="_______________|_________|_______|___________|"
+let d1="|"
+let doubleline="================================================"
+let curlline="   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+let star="*"
+console.log(star,doubleline)
+console.log("*                 ℝ𝔼ℙ𝕆ℝ𝕋 ℂ𝔸ℝ𝔻                      ")
+console.log(star,doubleline)
+console.log(star,school)
+console.log(star,curlline)
+console.log(star,"Name Of student :-  "+name)
+console.log(star,space)
+console.log(star,rollno,section)
+console.log(star,lowline)
+console.log(star,head1,d1,hmarksphysics,d1,hmarksmaths,d1,hmarkschemistry,d1)
+console.log(star,line)
+
+console.log(star,head2,d1,totalp,d1,totalm,d1,totalc,d1)
+console.log(star,head3,d1,marksphysics,d1,marksmaths,d1,markschemistry,d1)
+
+console.log(star,line)
+console.log(star,space)
+console.log(star,grade)
+console.log(star,space)
+console.log(star,sign,actsign)
+console.log(star,doubleline)
