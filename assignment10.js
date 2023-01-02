a4d6a5aca67b4d220f301b9728c73eca843c1089
Suraj12398 <surajdeosarkar99@gmail.com>
2023-01-02 23:24:39 +0530

    print_consonant

---
+++ assignment10.js
@@ -0,0 +1,4 @@
+//Problem 2 : Given any character, if it is a vowel print "Vowel"
+
+let character="a";
+if(character="a","e","i","o","u") { console.log("vowel")}
---
+++ assignment11.js
@@ -0,0 +1,4 @@
+//Problem 3 : Given and character if it is a consonant print "Consonant"
+
+let character="z";
+if(character!="a","e","i","o","u") { console.log("Consonant")}
