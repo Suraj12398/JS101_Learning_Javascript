90e85d6b0dac464ecef882128650d9e508faca1a
Suraj12398 <surajdeosarkar99@gmail.com>
2023-01-02 23:10:06 +0530

    multiple_of_3

---
+++ assignment5.js
@@ -0,0 +1,8 @@
+//Problem 1: If the number is divisible by 3, print a "multiple of 3".
+
+let number=6;
+
+if(number%3==0){
+  console.log("multiple of 3")
+}
+else{console.log("not multiple of 3")}
