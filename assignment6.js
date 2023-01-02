49d54fb5133889ce0537272209c4c62ac6d7ff44
Suraj12398 <surajdeosarkar99@gmail.com>
2023-01-02 23:14:13 +0530

    apply_for_license

---
+++ assignment6.js
@@ -0,0 +1,8 @@
+//Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
+
+let age=17;
+
+if(age>=18){
+  console.log("Apply for a locense")
+}
+else{console.log("NA")}
