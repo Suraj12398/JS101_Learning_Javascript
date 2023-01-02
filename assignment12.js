2264c44cfd633947439bb97d0f43e32ee6aa466d
Suraj12398 <surajdeosarkar99@gmail.com>
2023-01-02 23:26:22 +0530

    print_greatest

---
+++ assignment12.js
@@ -0,0 +1,9 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a=2;
+let b=3;
+let c=4;
+
+if(a>b && a>c) {console.log("a is greatest")};
+if(b>a && b>c) {console.log("b is greatest")};
+if(c>b && c>a) {console.log("c is greatest")};
